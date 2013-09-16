ea48737394c768db883e155681a599f04dcffff0
damienbod <damien_bod@hotmail.com>
2013-09-16 13:02:33 +0200

    init

---
+++ MvcUnityBootstrapperTest/Business/BusinessClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MvcUnityBootstrapperTest.Logging;
+
+namespace MvcUnityBootstrapperTest.Business
+{
+    public class BusinessClass : IBusinessClass
+    {
+        private IUnitOfWorkExample _unitOfWorkExample;
+
+        public BusinessClass(IUnitOfWorkExample unitOfWorkExample)
+        {
+            _unitOfWorkExample = unitOfWorkExample;
+            UnityEventLogger.Log.CreateUnityMessage("BusinessClass");
+        }
+
+        private bool _disposed = false;
+        public string Hello()
+        {
+            UnityEventLogger.Log.LogUnityMessage("Hello BusinessClass");
+            return "Hello from Business class";
+        }
+
+        public void Dispose()
+        {
+            _unitOfWorkExample.Dispose();
+            UnityEventLogger.Log.DisposeUnityMessage("BusinessClass");
+            if (!_disposed)
+            {
+                _disposed = true;
+            }
+        }
+    }
+}
---
+++ MvcUnityBootstrapperTest/UnityExtensions/UnityExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+using MvcUnityBootstrapperTest.Business;
+
+namespace Microsoft.Practices.Unity
+{
+    public static partial class WithLifetime
+    {
+        public static LifetimeManager PerRequest(Type type)
+        {
+            return new PerRequestLifetimeManager();
+        }
+    }
+}
+
