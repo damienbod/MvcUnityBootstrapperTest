07b5b2ca496de1e894d4228cbaa427570ce2882d
damienbod <damien_bod@hotmail.com>
2013-09-16 13:01:21 +0200

    init commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcUnityBootstrapperTest.Controllers
+{
+    public class HomeController : Controller
+    {          
+        private readonly Business.IBusinessClass _businessClass;
+
+        public HomeController(Business.IBusinessClass businessClass)
+        {
+            _businessClass = businessClass;
+        }
+
+        public ActionResult Index()
+        {
+            _businessClass.Hello();
+            return View();
+        }
+    }
+}
